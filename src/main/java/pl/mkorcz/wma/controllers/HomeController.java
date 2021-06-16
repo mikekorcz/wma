af375bcf7e07c3de2fb7043c45c47f716e38cf3f
Michal <mkorcz01@gmail.com>
2021-06-16 18:32:20 +0200

    HelloWorld test

---
+++ src/main/java/pl/mkorcz/wma/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package pl.mkorcz.wma.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    String displayHome() {
+        return "home";
+    }
+}
